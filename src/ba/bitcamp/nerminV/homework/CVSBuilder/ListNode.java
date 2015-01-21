03e462fd8acb673634589bf0ebc97706bae288e4
nerav <nermin.vucinic@gmail.com>
2015-01-21 23:52:32 +0100

    added zadaca.predavanje.15.1.15

---
+++ src/ba/bitcamp/nerminV/homework/CVSBuilder/ListNode.java
@@ -0,0 +1,17 @@
+package ba.bitcamp.nerminV.homework.CVSBuilder;
+/**
+* Node interface for read-only walking through list.
+*
+* @author emir
+*
+*/
+public interface ListNode {
+/**
+* @return value held by this node
+*/
+Object getValue();
+/**
+* @return next list node if available
+*/
+ListNode getNext();
+}
---
+++ src/ba/bitcamp/nerminV/homework/CVSBuilder/ValueWrapper.java
@@ -0,0 +1,23 @@
+package ba.bitcamp.nerminV.homework.CVSBuilder;
+/**
+* {@link ListNode} implementation.
+*
+* @author emir
+*
+*/
+public class ValueWrapper implements ListNode {
+private final Object value;
+private ValueWrapper next;
+public ValueWrapper(Object value) {
+this.value = value;
+}
+public ValueWrapper getNext() {
+return next;
+}
+public void setNext(ValueWrapper node) {
+next = node;
+}
+public Object getValue() {
+return value;
+}
+}
